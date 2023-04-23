9904452200e8cd4e19a0e20e051125ab5829cec3
nycan <ny.saskcan@gmail.com>
2023-04-22 21:07:18 -0600

    Added random things to formulas file

--- src/formulas.rs
+++ src/formulas.rs
@@ -1,6 +1,12 @@
+pub const GRAVITY:f64 = 6.674e-11_f64;
 pub const EARTH_GRAVITY:f64 = 398584628000000.0;
+pub const LIGHT_SPEED:f64 = 299792458.0;
 pub const EARTH_SEA_RADIUS:f64 = 6378137.0;
 pub const ROOM_TEMP:f64 = 288.15;
+
+pub fn gravity(mass:f64, distance:f64){
+    GRAVITY*mass/distance.powi(2)
+}
 
 pub fn earth_gravity(radius:f64) -> f64 {
     EARTH_GRAVITY/radius.powi(2)
