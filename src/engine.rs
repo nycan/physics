6333e387dc2e1360567c279bfc6b484e6e1f31c0
nycan <ny.saskcan@gmail.com>
2023-04-23 14:48:54 -0600

    Re-organized structure to be suitable for multiple objects

---
+++ src/engine.rs
@@ -0,0 +1,111 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::*;
+use piston::window::WindowSettings;
+const opengl : OpenGL = OpenGL::V3_2;
+
+pub const GRAVITY:f64 = 6.674e-11_f64;
+pub const EARTH_GRAVITY:f64 = 398584628000000.0;
+pub const LIGHT_SPEED:f64 = 299792458.0;
+pub const EARTH_SEA_RADIUS:f64 = 6378137.0;
+pub const ROOM_TEMP:f64 = 288.15;
+
+//formulas
+pub fn gravity(mass:f64, distance:f64) -> f64{GRAVITY*mass/distance.powi(2)}
+pub fn earth_gravity(radius:f64) -> f64 {EARTH_GRAVITY/radius.powi(2)}
+pub fn earth_pressure(radius:f64) -> f64 {101325.0 * (1.0-earth_gravity(radius)/289510.047).powf(3.50057557)}
+pub fn air_density(radius:f64, temp:f64) -> f64 {earth_pressure(radius)/(287.05*temp)}
+
+pub struct UpdateParams{
+    pub time:f64,
+    pub time_delta:f64,
+    pub paused:bool,
+    pub enable_gravity:bool,
+}
+
+pub trait Object{
+    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs, settings:&UpdateParams);
+    fn update(&mut self, settings:&UpdateParams);
+    fn reset(&mut self);
+    fn take_input(&mut self, key:Key);
+}
+
+pub struct Engine{
+    window: Window,
+    gl: opengl_graphics::GlGraphics,
+    objects: Vec<Box<dyn Object>>,
+    settings: UpdateParams,
+}
+
+impl Engine{
+    pub fn new() -> Engine{
+        Engine{
+            window: WindowSettings::new("vroooom", [640, 480])
+            .graphics_api(opengl).exit_on_esc(true).build().unwrap(),
+            gl: GlGraphics::new(opengl),
+            objects:Vec::new(),
+            settings:UpdateParams{
+                time:0.0,
+                time_delta:0.01,
+                paused:false,
+                enable_gravity:true,
+            }
+        }
+    }
+
+    fn reset(&mut self){
+        self.settings.time = 0.0;
+        for object in self.objects.iter_mut(){
+            object.reset();
+        }
+    }
+
+    fn update(&mut self){
+        for object in self.objects.iter_mut(){
+            object.update(&self.settings);
+        }
+        self.settings.time += self.settings.time_delta;
+    }
+
+    fn render(&mut self, args: &RenderArgs){
+        for object in self.objects.iter_mut(){
+            object.render(&mut self.gl, args, &self.settings);
+        }
+    }
+
+    pub fn add_object<T: Object + 'static>(&mut self, object:T){
+        self.objects.push(Box::new(object));
+    }
+
+    pub fn run(&mut self){
+        let mut events = Events::new(EventSettings::new());
+        while let Some(e) = events.next(&mut self.window) {
+            if let Some(args) = e.render_args() {
+                self.render(&args);
+            }
+
+            if let Some(args) = e.update_args() {
+                self.settings.time_delta = if self.settings.paused {0.0} else {args.dt};
+                self.update();
+            }
+
+            if let Some(Button::Keyboard(key)) = e.press_args(){
+                match key{
+                    Key::R => self.reset(),
+                    Key::Space => self.settings.paused = !self.settings.paused,
+                    Key::G => self.settings.enable_gravity = !self.settings.enable_gravity,
+                    _ => {}
+                }
+                for object in self.objects.iter_mut(){
+                    object.take_input(key);
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
-
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::*;
-use piston::window::WindowSettings;
-
 #[allow(dead_code)]
-mod formulas;
-use formulas::*;
+mod engine;
+use engine::*;
+use piston::{RenderArgs,Key};
+use opengl_graphics::GlGraphics;
 
-const SURFACE_TEMP: f64 = ROOM_TEMP;
-const SURFACE: f64 = EARTH_SEA_RADIUS;
+const SURFACE_TEMP: f64 = 288.15;
+const SURFACE: f64 = 6378137.0;
 
 pub struct Rocket {
-    gl: opengl_graphics::GlGraphics, // OpenGL drawing backend.
     // Changing variables
     mass:f64,
     velocity:[f64;2],
     pos:[f64;2],
-    time:f64,
 
     // Constants
     exhaust_velocity:[f64;2],
@@ -33,14 +22,11 @@ pub struct Rocket {
     // Settings
     enable_thrust:bool,
     enable_drag:bool,
-    enable_gravity:bool,
     thrust_time:f64,
-    paused:bool,
-    time_delta:f64,
 }
 
-impl Rocket{
-    fn render(&mut self, args: &RenderArgs){
+impl Object for Rocket{
+    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs, settings:&UpdateParams){
         use graphics::*;
 
         const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
@@ -61,65 +47,65 @@ impl Rocket{
         );
         let ground = [0.0, args.window_size[1], args.window_size[0], -20.0*scale];
         
-        self.gl.draw(args.viewport(), |c, gl| {
+        gl.draw(args.viewport(), |c, thingy| {
             // Clear the screen.
-            clear(WHITE, gl);
+            clear(WHITE, thingy);
 
-            rectangle(GREEN, ground, c.transform, gl);
-            rectangle(BLACK, rocket, c.transform, gl);
-            if (self.time<=self.thrust_time) && (self.enable_thrust) {
-                rectangle(ORANGE, flame, c.transform, gl);
+            rectangle(GREEN, ground, c.transform, thingy);
+            rectangle(BLACK, rocket, c.transform, thingy);
+            if (settings.time<=self.thrust_time) && (self.enable_thrust) {
+                rectangle(ORANGE, flame, c.transform, thingy);
             }
         });
     }
 
-    fn update(&mut self, args: &UpdateArgs){ // DE Source: https://web.mit.edu/16.unified/www/FALL/systems/Lab_Notes/traj.pdf
-        self.time_delta = args.dt;
-        println!("{}", self.time_delta);
-        
-        if (self.pos[1] < 0.0) || (self.paused) {
+    fn update(&mut self, settings:&UpdateParams){ // DE Source: https://web.mit.edu/16.unified/www/FALL/systems/Lab_Notes/traj.pdf
+        if self.pos[1] < 0.0 {
             return;
         }
 
         let temp = SURFACE_TEMP - 0.0065*(self.pos[1]+SURFACE-EARTH_SEA_RADIUS);
         let density = air_density(self.pos[1]+SURFACE, temp);
-        self.pos[0] += self.time_delta*self.velocity[0];
-        self.pos[1] += self.time_delta*self.velocity[1];
-        self.velocity[0] += self.time_delta * (
+        self.pos[0] += settings.time_delta*self.velocity[0];
+        self.pos[1] += settings.time_delta*self.velocity[1];
+        self.velocity[0] += settings.time_delta * (
             -0.5*density*self.velocity[0]*self.velocity[0]*self.drag_coeff*self.cross_section/self.mass
             *(if self.enable_drag {1.0} else {0.0})
         );
-        self.velocity[1] += self.time_delta * (
-            -earth_gravity(self.pos[1]+SURFACE)*(if self.enable_gravity {1.0} else { 0.0 })
+        self.velocity[1] += settings.time_delta * (
+            if settings.enable_gravity{-earth_gravity(self.pos[1]+SURFACE)} else {0.0}
             -0.5*density*self.velocity[1]*self.velocity[1]*self.drag_coeff*self.cross_section/self.mass
             *(if self.enable_drag {1.0} else {0.0})
         );
-        if (self.time<=self.thrust_time) && (self.enable_thrust){
-            self.velocity[0] += self.time_delta * (self.mass_flow_rate*self.exhaust_velocity[0]/self.mass);
-            self.velocity[1] += self.time_delta * (self.mass_flow_rate*self.exhaust_velocity[1]/self.mass);
-            self.mass -= self.time_delta * self.mass_flow_rate;
+        if (settings.time<=self.thrust_time) && (self.enable_thrust){
+            self.velocity[0] += settings.time_delta * (self.mass_flow_rate*self.exhaust_velocity[0]/self.mass);
+            self.velocity[1] += settings.time_delta * (self.mass_flow_rate*self.exhaust_velocity[1]/self.mass);
+            self.mass -= settings.time_delta * self.mass_flow_rate;
         }
-
-        self.time += self.time_delta;
     }
 
     fn reset(&mut self){
         self.mass = 0.2;
         self.velocity = [0.0, 0.0];
         self.pos = [0.0, 0.0];
-        self.time = 0.0;
+    }
+
+    fn take_input(&mut self, key:Key){
+
+        match key{
+            Key::Left => self.exhaust_velocity[0] -= 100.0,
+            Key::Right => self.exhaust_velocity[0] += 100.0,
+            Key::T => self.enable_thrust = !self.enable_thrust,
+            Key::D => self.enable_drag = !self.enable_drag,
+            _ => {},
+        }
     }
 }
 
 fn main() {
-    let opengl = OpenGL::V3_2;
+    let mut engine = Engine::new();
 
-    let mut window: Window = 
-        WindowSettings::new("vroooom", [640, 480])
-        .graphics_api(opengl).exit_on_esc(true).build().unwrap();
-
-    let mut rocket = Rocket {
-        gl: GlGraphics::new(opengl),
+    let rocket = Rocket {
         mass: 0.2,
         velocity: [0.0, 0.0],
         pos: [0.0, 0.0],
@@ -127,35 +113,11 @@ fn main() {
         drag_coeff: 0.1,
         cross_section: 0.01,
         mass_flow_rate: 0.01,
-        time: 0.0,
         enable_thrust: true,
         enable_drag: true,
-        enable_gravity: true,
         thrust_time: 4.5,
-        paused: false,
-        time_delta: 0.01,
     };
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            rocket.render(&args);
-        }
 
-        if let Some(args) = e.update_args() {
-            rocket.update(&args);
-        }
-
-        if let Some(Button::Keyboard(key)) = e.press_args(){
-            match key{
-                Key::R => rocket.reset(),
-                Key::Space => rocket.paused = !rocket.paused,
-                Key::Left => rocket.exhaust_velocity[0] -= 100.0,
-                Key::Right => rocket.exhaust_velocity[0] += 100.0,
-                Key::T => rocket.enable_thrust = !rocket.enable_thrust,
-                Key::D => rocket.enable_drag = !rocket.enable_drag,
-                Key::G => rocket.enable_gravity = !rocket.enable_gravity,
-                _ => {}
-            }
-        }
-    }
+    engine.add_object(rocket);
+    engine.run();
 }
