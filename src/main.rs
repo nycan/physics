e371ad54e82b1282231e580218e603f1124b95c7
Derek Hoffman <fantasticpancakeman@gmail.com>
2023-04-23 16:24:07 -0600

    added a separate type of object called IFO for simulating ballistics without thrust

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,25 @@ pub struct Rocket {
     thrust_time:f64,
 }
 
+pub struct IFO {
+    // Changing variables
+    velocity:[f64;2],
+    pos:[f64;2],
+
+    // Constants
+    mass:f64,
+    drag_coeff:f64,
+    cross_section:f64,
+
+    // Settings
+    enable_drag:bool,
+}
+
 impl Object for Rocket{
     fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs, settings:&UpdateParams){
         use graphics::*;
 
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        //const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
         const ORANGE: [f32; 4] = [1.0, 0.5, 0.0, 1.0];
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
@@ -102,6 +116,65 @@ impl Object for Rocket{
     }
 }
 
+impl Object for IFO {
+    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs, settings:&UpdateParams){
+        use graphics::*;
+
+        // const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        let ifo = rectangle::square(
+            args.window_size[0]/2.0-25.0*settings.scale+self.pos[0]*5.0*settings.scale,
+            args.window_size[1]-(self.pos[1]*5.0+50.0)*settings.scale,50.0*settings.scale
+        );
+        let ground = [0.0, args.window_size[1], args.window_size[0], -20.0*settings.scale];
+
+        gl.draw(args.viewport(), |c, thingy| {
+
+            rectangle(GREEN, ground, c.transform, thingy);
+            rectangle(BLACK, ifo, c.transform, thingy);
+        });
+    }
+
+    fn scale(&self, args: &RenderArgs) -> f64 {
+        ((args.window_size[1]-75.0)/(5.0*self.pos[1])).abs()
+        .min(((args.window_size[0]/2.0-50.0)/(5.0*self.pos[0].abs())).abs())
+    }
+
+    fn update(&mut self, settings:&UpdateParams){
+        if self.pos[1] < 0.0 {
+            return;
+        }
+
+        let temp = SURFACE_TEMP - 0.0065*(self.pos[1]+SURFACE-EARTH_SEA_RADIUS);
+        let density = air_density(self.pos[1]+SURFACE, temp);
+        self.pos[0] += settings.time_delta*self.velocity[0];
+        self.pos[1] += settings.time_delta*self.velocity[1];
+        self.velocity[1] += settings.time_delta * (
+            if settings.enable_gravity{-earth_gravity(self.pos[1]+SURFACE)} else {0.0}
+            -0.5*density*self.velocity[1]*self.velocity[1]*self.drag_coeff*self.cross_section/self.mass
+            *(if self.enable_drag {1.0} else {0.0})
+        );
+        self.velocity[0] += settings.time_delta * (
+            -0.5*density*self.velocity[0]*self.velocity[0]*self.drag_coeff*self.cross_section/self.mass
+            *(if self.enable_drag {1.0} else {0.0})
+        );
+    }
+
+    fn reset(&mut self){
+        self.velocity=[0.0,0.0];
+        self.pos=[0.0,0.0]
+    }
+
+    fn take_input(&mut self, key:Key){
+        match key{
+            Key::D => self.enable_drag = !self.enable_drag,
+            _ => {},
+        }
+    }
+}
+
 fn main() {
     let mut engine = Engine::new();
 
@@ -132,6 +205,16 @@ fn main() {
         enable_drag: true,
         thrust_time: 4.5,
     };
+
+    let ifo:IFO = IFO {
+        mass: 1.0,
+        velocity: [100.0,100.0],
+        pos: [-100.0,0.0],
+        drag_coeff: 1.0,
+        cross_section: 0.01,
+        enable_drag: true,
+    };
+    engine.add_object(ifo);
 
     engine.add_object(rocket2);
     engine.run();
